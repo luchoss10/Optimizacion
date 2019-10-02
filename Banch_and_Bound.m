155d7f1edc1807dcde3eae1ca95686f4fb433a22
luchoss10 <lucncd@gmail.com>
2019-10-02 14:47:44 -0500

    Renomenclatua_Variables
    Se renombras varias variables para el entendimiento.

--- Banch_and_Bound.m
+++ Banch_and_Bound.m
@@ -1,26 +1,34 @@
-function [xstar,Jmin] = Banch_and_Bound(c,A,b,xlb,xub,xstar,Jmin)
-% Integer linear programming by (recursive) branch & bound
-% This algorithm uses linprog.m from the optimisation toolbox
-    tol = 1e-5; % anything less is considered zero
-    if ~exist('xlb','var'), xlb = 0*c; end % canonical form (for the moment)
-    if ~exist('xub','var'), xub = Inf*ones(size(c)); end % Upper bound defaults
-    if ~exist('xstar','var'), xstar = []; end
-    if ~exist('Jmin','var'), Jmin = Inf; end % Try to minimise this
-        % Use the following two line if you prefer OPTI Optimization toolbox
-        % LPrelax = opti(’f’,c,’ineq’,A,b, ’bounds’, xlb, xub);
-        % [x,j,exitflag] = solve(LPrelax);
-    optns = optimset('display','off'); % turn off diagnostics in Linprog
-    [x,j,exitflag] = linprog(c,A,b,[],[],xlb,xub,[],optns);
-    if exitflag~=1, return, end % if infeasible, branch ended
-    if j>Jmin, return, end % if current cost J=cT x is worse, drop
-    idx = find(abs(x-round(x)) > tol); % which are non-integers ?
-    if isempty(idx) % All integer solutions
-        if j<Jmin, xstar=round(x); Jmin=j; end % New integer optimum
+%Programacion lineal entera metodo de Branch and Bound por medio de
+%recursividad
+function [xInicial,FOmin] = Banch_and_Bound(c,A,b,xlb,xub,xInicial,FOmin)
+% El algoritmo emplea la funcion linprog de matlab
+    tolerancia = 1e-5; % Cualquier numero por debajo de este valor es considerado cero
+    if ~exist('xlb','var'), xlb = 0*c; end % Forma canonica
+    if ~exist('xub','var'), xub = Inf*ones(size(c)); end % Limite superior predeterminado
+    if ~exist('xstar','var'), xInicial = []; end
+    if ~exist('Jmin','var'), FOmin = Inf; end % Intento de minimizacion
+    optns = optimset('display','off'); % Desagtiva el diagnostico en linprog
+    [x,j,salida] = linprog(c,A,b,[],[],xlb,xub,[],optns);
+    if salida~=1, return, end % Si es infactible, se elimina la rama
+    if j>FOmin, return, end % Si el valor de FO es el mismo que el incurrente sale de la rama
+    idx = find(abs(x-round(x)) > tolerancia); % Determina que valores no son enteros
+    if isempty(idx) % Todos las soluciones Enteras
+        if j<FOmin, xInicial=round(x); FOmin=j; end % Nuevo optimo Entero
         return % problem solved
     end
-    % Depth first recursion
-    xnlb = xlb; xnlb(idx(1)) = ceil(x(idx(1))); % Take x<--int. greater than x
-    [xstar,Jmin] = Banch_and_Bound(c,A,b,xnlb,xub,xstar,Jmin);
-    xnub = xub; xnub(idx(1)) = floor(x(idx(1))); % Take x<--int. less than x
-    [xstar,Jmin] = Banch_and_Bound(c,A,b,xlb,xnub,xstar,Jmin);
+    % Recursion del metodo por aca nuevo nodo con sus nuevas restricciones.
+    xnlb = xlb; xnlb(idx(1)) = ceil(x(idx(1))); % Toma el x<--entero. mayor
+    [xInicial,FOmin] = Banch_and_Bound(c,A,b,xnlb,xub,xInicial,FOmin);
+    xnub = xub; xnub(idx(1)) = floor(x(idx(1))); % Toma el x<--entero. menor
+    [xInicial,FOmin] = Banch_and_Bound(c,A,b,xlb,xnub,xInicial,FOmin);
 end
+
+%Ejemplo
+%c=[1;10]
+%A=[-66 -14;82 -28]
+%b=[-1430;-1306]
+%Solucion No Entera
+%[xreal,jreal]=linprog(c,A,b)
+%Solucion Entera Usando Branch_and_Bound
+%[xEntero,FOentera] = Banch_and_Bound(c,A,b)
+
